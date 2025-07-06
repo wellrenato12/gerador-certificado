94e9fd92fb9740f9529f312066472a3e9571818a
Wellington Albino <well.renato@hotmail.com>
2025-07-06 11:10:31 -0300

    Criando primary button

---
+++ src/app/_components/primary-button/primary-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  templateUrl: './primary-button.component.html',
+  styleUrl: './primary-button.component.css'
+})
+export class PrimaryButtonComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
