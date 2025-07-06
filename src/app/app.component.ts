f18a182cb9d381d8eddebbf5ce7c27517336a2fe
Wellington Albino <well.renato@hotmail.com>
2025-07-06 13:02:23 -0300

    Criando página de certificado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./_components/navbar/navbar.component";
-import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
-import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
-import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
-import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
-import { CertificadosComponent } from "./pages/certificados/certificados.component";
-import { CertificadosFormComponent } from "./pages/certificados-form/certificados-form.component";
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+import { ItemCertificadoComponent } from './_components/item-certificado/item-certificado.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
+import { CertificadoComponent } from './pages/certificado/certificado.component';
+import { CertificadosFormComponent } from './pages/certificados-form/certificados-form.component';
+import { CertificadosComponent } from './pages/certificados/certificados.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent, BaseUiComponent, CertificadosComponent, CertificadosFormComponent],
+  imports: [
+    NavbarComponent,
+    CommonModule,
+    PrimaryButtonComponent,
+    SecondaryButtonComponent,
+    ItemCertificadoComponent,
+    BaseUiComponent,
+    CertificadosComponent,
+    CertificadosFormComponent,
+    CertificadoComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'gerador-certificado';
-  exibeNavbar: boolean = false
+  exibeNavbar: boolean = true;
 }
---
+++ src/app/pages/certificado/certificado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+
+@Component({
+  selector: 'app-certificado',
+  imports: [SecondaryButtonComponent],
+  templateUrl: './certificado.component.html',
+  styleUrl: './certificado.component.css',
+})
+export class CertificadoComponent {}
