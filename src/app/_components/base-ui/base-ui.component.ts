ce156136a90c6899b49086520d36b7e2bda59b10
Wellington Albino <well.renato@hotmail.com>
2025-07-06 11:38:48 -0300

    Criando a interface base

---
+++ src/app/_components/base-ui/base-ui.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-ui',
+  imports: [],
+  templateUrl: './base-ui.component.html',
+  styleUrl: './base-ui.component.css'
+})
+export class BaseUiComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
 import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
 import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
+import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent],
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent, BaseUiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
