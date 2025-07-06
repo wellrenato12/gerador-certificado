57f3e908ead1a7cea6cfa22d977bde0363ab9718
Wellington Albino <well.renato@hotmail.com>
2025-07-06 11:31:34 -0300

    Criando item certificado

---
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from "../secondary-button/secondary-button.component";
+
+@Component({
+  selector: 'app-item-certificado',
+  imports: [SecondaryButtonComponent],
+  templateUrl: './item-certificado.component.html',
+  styleUrl: './item-certificado.component.css'
+})
+export class ItemCertificadoComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
 import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
+import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
