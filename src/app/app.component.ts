0713f6e30dad4a2712e9cf743c6f7ef8ca77f853
Wellington Albino <well.renato@hotmail.com>
2025-07-06 11:17:38 -0300

    Criando secondary button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
+import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent],
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
