bbedb783d211e2f5a56294ef456df0112364244e
Wellington Albino <well.renato@hotmail.com>
2025-07-09 10:32:27 -0300

    Usando router link para navegar entre as paginas

--- src/app/_components/item-certificado/item-certificado.component.ts
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../secondary-button/secondary-button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecondaryButtonComponent],
+  imports: [
+    SecondaryButtonComponent,
+    RouterLink,
+  ],
   templateUrl: './item-certificado.component.html',
   styleUrl: './item-certificado.component.css'
 })
 export class ItemCertificadoComponent {
-
+  id: string = '6'
 }
--- src/app/pages/certificado/certificado.component.ts
+++ src/app/pages/certificado/certificado.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certificado',
-  imports: [SecondaryButtonComponent],
+  imports: [
+    SecondaryButtonComponent,
+    RouterLink
+  ],
   templateUrl: './certificado.component.html',
   styleUrl: './certificado.component.css',
 })
