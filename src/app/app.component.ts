92f7bd23dab7487669b10baf382d05b9de66e755
Wellington Albino <well.renato@hotmail.com>
2025-07-09 10:15:46 -0300

    Definindo as rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BaseUiComponent } from './_components/base-ui/base-ui.component';
-import { ItemCertificadoComponent } from './_components/item-certificado/item-certificado.component';
+
 import { NavbarComponent } from './_components/navbar/navbar.component';
-import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
-import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
-import { CertificadoComponent } from './pages/certificado/certificado.component';
-import { CertificadosFormComponent } from './pages/certificados-form/certificados-form.component';
-import { CertificadosComponent } from './pages/certificados/certificados.component';
+
+
+
+
+
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
     NavbarComponent,
     CommonModule,
-    PrimaryButtonComponent,
-    SecondaryButtonComponent,
-    ItemCertificadoComponent,
     BaseUiComponent,
-    CertificadosComponent,
-    CertificadosFormComponent,
-    CertificadoComponent,
+    RouterOutlet,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- src/app/pages/certificados/certificados.component.ts
+++ src/app/pages/certificados/certificados.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { ItemCertificadoComponent } from '../../_components/item-certificado/item-certificado.component';
-import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+
 
 @Component({
   selector: 'app-certificados',
-  imports: [SecondaryButtonComponent, ItemCertificadoComponent],
+  imports: [ItemCertificadoComponent],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.css',
 })
