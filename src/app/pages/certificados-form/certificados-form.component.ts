acf57888c263c694742a3b653c46722bd50e6918
Wellington Albino <well.renato@hotmail.com>
2025-07-11 15:29:06 -0300

    Aprendendo sobre ngModel

--- src/app/pages/certificados-form/certificados-form.component.ts
+++ src/app/pages/certificados-form/certificados-form.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificados-form',
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FormsModule],
   templateUrl: './certificados-form.component.html',
   styleUrl: './certificados-form.component.css',
 })
-export class CertificadosFormComponent {}
+export class CertificadosFormComponent {
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React'];
+}
