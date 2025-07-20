53929ee6721e1c71e356c05f3e20af8034588c5f
Wellington Albino <well.renato@hotmail.com>
2025-07-20 12:20:57 -0300

    Exibindo as informações

--- src/app/_services/certificado.service.ts
+++ src/app/_services/certificado.service.ts
@@ -11,7 +11,7 @@ export class CertificadoService {
   constructor() { }
 
   adicionarCertificado(certificado: Certificado) {
-    this.certificados.push({...certificado});
+    this.certificados.unshift({...certificado});
     localStorage.setItem('certificados', JSON.stringify(this.certificados))
   }
 }
