34ae02d7d0d2b50191968a3947c5daa90be0f881
Wellington Albino <well.renato@hotmail.com>
2025-07-20 12:36:33 -0300

    finalizando aplicacao

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
@@ -12,8 +12,6 @@ import { RouterLink, RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements OnInit {
-  ngOnInit(): void {
-    console.log('Meu componente navbar inicializou')
-  }
+export class NavbarComponent {
+
 }
